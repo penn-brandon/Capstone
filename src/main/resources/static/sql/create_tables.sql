8a8526ac69acc8b231497b8d5eb5333eb03d6544
Brandon <143059277+penn-brandon@users.noreply.github.com>
2024-10-01 00:16:31 -0400

    fixed all present issues
    Hopefully this is the final draft of the sql script for our mysql database

--- src/main/resources/static/sql/create_tables.sql
+++ src/main/resources/static/sql/create_tables.sql
@@ -6,33 +6,33 @@ use capstone;
 
 create table permission
 (
-    permission_id   int AUTO_INCREMENT primary key,
+    permission_id   int auto_increment primary key not null,
     permission_name VARCHAR(255)
 );
 
 create table chat_room
 (
-    chat_room_id int AUTO_INCREMENT primary key,
-    can_join     bool,
+    chat_room_id int auto_increment primary key not null,
+    can_join     bool default false,
     members      LONGBLOB,
     message_list LONGBLOB
 );
 
 create table user
 (
-    user_id          int AUTO_INCREMENT primary key,
+    user_id          int auto_increment primary key not null,
     username         VARCHAR(255) not null,
     password         VARCHAR(255) not null,
-    date_of_creation datetime default NOW(),
-    permission       int,
+    date_of_creation datetime default NOW() not null,
+    permission       int default 0 not null,
     chatRoomList     LONGBLOB,
     FOREIGN KEY (permission) REFERENCES permission (permission_id)
 );
 
 create table chat_room_name
 (
-    user           int,
-    chat_room      integer,
+    user           int not null,
+    chat_room      int not null,
     ownership      bool,
     chat_room_name varchar(255),
     FOREIGN KEY (user) REFERENCES user (user_id),
@@ -41,20 +41,20 @@ create table chat_room_name
 
 create table connections
 (
-    user       int,
-    connection int,
-    block      bool,
+    user       int not null,
+    connection int not null,
+    block      bool default false not null,
     FOREIGN KEY (user) REFERENCES user (user_id),
     FOREIGN KEY (connection) REFERENCES user (user_id)
 );
 
 create table message
 (
-    message_id serial primary key,
-    content    blob,
-    date       datetime,
-    chatroom   int,
-    sender     int,
+    message_id int auto_increment primary key not null,
+    content    blob not null,
+    date       datetime default NOW() not null,
+    chatroom   int not null,
+    sender     int not null,
     FOREIGN KEY (chatroom) REFERENCES chat_room (chat_room_id),
     FOREIGN KEY (sender) REFERENCES user (user_id)
 );
