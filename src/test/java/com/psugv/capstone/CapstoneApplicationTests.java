39816cc2c859d4a5f69f755f5f120fa67f1e4990
WeiChuanWilliam <wei.chuan.william@gmail.com>
2024-11-20 01:16:40 -0500

    Test auto-testing

--- src/test/java/com/psugv/capstone/CapstoneApplicationTests.java
+++ src/test/java/com/psugv/capstone/CapstoneApplicationTests.java
@@ -1,6 +1,8 @@
 /*package com.psugv.capstone;
 
+import com.psugv.capstone.service.ChatServiceTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -12,5 +14,4 @@ class CapstoneApplicationTests {
 
     }
 
-}
-*/
+}*/
