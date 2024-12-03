6b06d101abb62c220d807fe25b4574743a764b11
WeiChuanWilliam <wei.chuan.william@gmail.com>
2024-12-02 23:22:36 -0500

    Test

--- src/main/java/com/psugv/capstone/CapstoneApplication.java
+++ src/main/java/com/psugv/capstone/CapstoneApplication.java
@@ -2,9 +2,10 @@ package com.psugv.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CapstoneApplication {
+public class CapstoneApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
